74b643ce705ee23d86eada2a7864fe2a68e521bd
LuisGonzalez <luis.rigoma@gmail.com>
2017-05-12 23:13:57 -0600

    Subiendo tarea

---
+++ Tarea1_CRUD/Tarea1_CRUD/Controllers/EmpleadoController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tarea1_CRUD;
+
+namespace Tarea1_CRUD.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Empleado
+        public ActionResult Index()
+        {
+            var empleado = db.Empleado.Include(e => e.Departamento);
+            return View(empleado.ToList());
+        }
+
+        // GET: Empleado/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleado/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdDepto = new SelectList(db.Departamento, "Id", "NombreDepartamento");
+            return View();
+        }
+
+        // POST: Empleado/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Apellido,FechaNacimiento,Sueldo,IdDepto")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empleado.Add(empleado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdDepto = new SelectList(db.Departamento, "Id", "NombreDepartamento", empleado.IdDepto);
+            return View(empleado);
+        }
+
+        // GET: Empleado/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdDepto = new SelectList(db.Departamento, "Id", "NombreDepartamento", empleado.IdDepto);
+            return View(empleado);
+        }
+
+        // POST: Empleado/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Apellido,FechaNacimiento,Sueldo,IdDepto")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdDepto = new SelectList(db.Departamento, "Id", "NombreDepartamento", empleado.IdDepto);
+            return View(empleado);
+        }
+
+        // GET: Empleado/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleado/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleado empleado = db.Empleado.Find(id);
+            db.Empleado.Remove(empleado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Tarea1_CRUD/Tarea1_CRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tarea1_CRUD.Startup))]
+namespace Tarea1_CRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
